bda0340e0bb29226d494e9f6b2abcacbc0b40a76
Michael Preston <MichaelPreston@bayviewloans.com>
2020-05-18 20:33:13 -0500

    WiP - Account page to show worked-on posts, drafts, for user

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { Provider as MobxProvider } from 'mobx-react'
 import App from 'next/app'
 import React from 'react'
 import { withAuthorization } from '../services/firebase'
-import { fetchInitialStoreState, Store } from '../stores/root'
+import { fetchInitialStoreState, Store, store } from '../stores/root'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 // import { DialogProvider } from 'muibox'
@@ -10,14 +10,17 @@ import DialogProvider from '../hoc/DialogProvider'
 
 class MobxApp extends App {
   state = {
-    store: new Store(),
-  }
+    // store: new Store(),
+    store
+  };
+
+  componentDidMount = () => console.count('_App init()')
 
   // Fetching serialized(JSON) store state
   static async getInitialProps(appContext) {
     const appProps = await App.getInitialProps(appContext)
     const initialStoreState = await fetchInitialStoreState()
-
+    console.log('initialStoreState', initialStoreState)
     return {
       ...appProps,
       initialStoreState,
@@ -39,7 +42,7 @@ class MobxApp extends App {
           <AuthorizedApp {...pageProps} />
           <ToastContainer newestOnTop />
         </DialogProvider>
-      </MobxProvider >
+      </MobxProvider>
     )
   }
 }
