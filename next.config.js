d6067607f4585a943f65fb8de9dab735e087e2db
Braden Preston <bpfilmsinc@gmail.com>
2020-06-14 09:27:19 -0500

    Local package of custom ckeditor build added

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 const withCSS = require('@zeit/next-css')
 require('dotenv').config()
 const webpack = require('webpack')
+const path = require('path')
+
+console.log('cat')
 
 module.exports = withCSS({    
     webpack(config) {
         config.plugins.push(new webpack.EnvironmentPlugin(process.env))
+
+        // Local module path resolutions. These may end on NPM at some point / moved to another repo
+        config.resolve.alias['tpot-scribe-editor'] = path.resolve(__dirname, 'packages/tpot-scribe-editor')
+        config.resolve.alias['ckeditor5-build-fully-featured'] = path.resolve(__dirname, 'packages/ckeditor5-build-fully-featured')
+
         return config
     },
 })
