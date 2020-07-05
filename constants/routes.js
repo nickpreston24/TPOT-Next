27bd2b8078f76db7aab5fe57204f61ccc0a842ae
Braden Preston <bpfilmsinc@gmail.com>
2020-07-05 17:23:58 -0500

    Allow log out ability from header with redirect
    Additional UI added as well for a user menu dropdown. 
    
    There appear to be some problem with ProvideAuth handleAuthFailure(). The redirect does not occur when the user signs out (without a manual redirect callback)

--- constants/routes.js
+++ constants/routes.js
@@ -6,3 +6,4 @@ export const EDIT = '/scribe/edit'
 export const DOC = (docId) => `/scribe/edit/${docId}`
 export const CHECKOUT = '/scribe/checkout'
 export const TPOT = 'http://www.thepathoftruth.com'
+export const TRELLO = 'https://trello.com/c/LsaGjczl/99-tpot-letters-desktop-app'
