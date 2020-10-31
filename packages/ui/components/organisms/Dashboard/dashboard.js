e754269f81bef6085cfb5066087b6ab3e53a4945
Braden Preston <bpfilmsinc@gmail.com>
2020-10-31 15:37:07 -0500

    prototype the publish details panel

--- packages/ui/components/organisms/Dashboard/dashboard.js
+++ packages/ui/components/organisms/Dashboard/dashboard.js
@@ -1,4 +1,22 @@
-import { Box, Flex, Icon, Stack } from "@chakra-ui/core";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Icon,
+  IconButton,
+  Input,
+  PseudoBox,
+  Select,
+  Stack,
+  Tag,
+  TagCloseButton,
+  TagLabel,
+  Text,
+  useTheme,
+} from "@chakra-ui/core";
 
 import ListItem from "@molecules/ListItem";
 import Navbar from "@organisms/Navbar";
@@ -28,14 +46,7 @@ const Dashboard = ({ title, ...props }) => {
               h={600}
               bg="white"
             ></Box>
-            <Box
-              borderRadius="lg"
-              boxShadow="md"
-              d={{ base: "none", lg: "block" }}
-              w={{ md: 500 }}
-              h={400}
-              bg="white"
-            ></Box>
+            <DetailsPanel />
           </Stack>
           <DetailsToggle />
         </Box>
@@ -58,6 +69,8 @@ Dashboard.defaultProps = {
 
 export default Dashboard;
 
+const tagOptions = ["chinese", "diabolical doctrine", "salvation", "faith"];
+
 const DetailsToggle = () => (
   <Flex
     right="-16px"
@@ -77,4 +90,121 @@ const DetailsToggle = () => (
   >
     <Icon name="info" />
   </Flex>
+);
+
+const DetailsPanel = () => (
+  <Stack
+    spacing={4}
+    borderRadius="lg"
+    boxShadow="md"
+    d={{ base: "none", lg: "block" }}
+    boxSizing="border-box"
+    p={6}
+    minW={{ base: 0, md: 350 }}
+    h="100%"
+    bg="white"
+    color="gray.600"
+  >
+    <CardTitle />
+    <Stack direction="row">
+      <Box width={1 / 2}>
+        <FormLabel htmlFor="type">Type</FormLabel>
+        <Select
+          id="type"
+          value="translation"
+          onChange={() => null}
+          variant="filled"
+        >
+          <option value="letter">Letter</option>
+          <option value="translation">Translation</option>
+          <option value="biography">Biography</option>
+          <option value="page">Generic</option>
+        </Select>
+      </Box>
+      <Box width={1 / 2}>
+        <FormLabel htmlFor="tags">Tags</FormLabel>
+        <Select
+          id="type"
+          defaultValue=""
+          onChange={() => null}
+          variant="filled"
+        >
+          <option value="">Select...</option>
+          <option value="translation">Translation</option>
+          <option value="biography">Biography</option>
+          <option value="page">Generic</option>
+        </Select>
+      </Box>
+    </Stack>
+    <Box>
+      {tagOptions.map((tag, idx) => (
+        <Tag
+          key={idx}
+          rounded="full"
+          size="sm"
+          variantColor="teal"
+          mr={1}
+          mb={1}
+        >
+          <TagLabel>{tag}</TagLabel>
+          <TagCloseButton />
+        </Tag>
+      ))}
+    </Box>
+    <Box>
+      <FormLabel htmlFor="slug">Slug</FormLabel>
+      <Stack direction="row">
+        <Input id="slug" placeholder="name-of-paper" variant="filled" />
+      </Stack>
+    </Box>
+    <Box>
+      <Stack direction="row">
+        <FormLabel htmlFor="slug" flexGrow={1}>
+          When live, the URL will be:
+        </FormLabel>
+        <Button size="xs">Copy</Button>
+      </Stack>
+      <PseudoBox
+        mt={1}
+        rounded="full"
+        transition="250ms background ease-in"
+        _hover={{ bg: "blue.100" }}
+        _active={{ bg: "blue.100" }}
+      >
+        <Text
+          px={4}
+          as="input"
+          w="100%"
+          outline="none"
+          color="blue.500"
+          bg="transparent"
+          textDecor="underline"
+          fontSize="sm"
+          readOnly
+          contentEditable
+          isTruncated
+          value="www.thepathoftruth.com/letters/name-of-paper.htm"
+        />
+      </PseudoBox>
+    </Box>
+    <Stack direction="row" pt={2} spacing={0}>
+      <Button variantColor="blue" w="50%">
+        Save
+      </Button>
+      <Button variantColor="green" w="100%">
+        Submit for Review
+      </Button>
+    </Stack>
+  </Stack>
+);
+
+const CardTitle = () => (
+  <Stack pb={4}>
+    <Stack direction="row">
+      <Heading size="md" mt={1} fontWeight={500}>
+        Publish Details
+      </Heading>
+    </Stack>
+    <Box h="2px" bg="gray.100" />
+  </Stack>
 );
