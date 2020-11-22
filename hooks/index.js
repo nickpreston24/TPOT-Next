75749e7c972ae5bb8c394f8110b3664f9d02e848
Michael <michael.n.preston@gmail.com>
2020-11-22 15:03:11 -0600

    Added uselocalstorage hook

--- hooks/index.js
+++ hooks/index.js
@@ -5,5 +5,4 @@ export { usePrevious } from './usePrevious'
 export { useBeforeUnload, usePreventWindowUnload } from './useBeforeUnload'
 export { ProvideSessions, useSessions } from './useSessions'
 export { useDebounce } from './useDebounce'
-
 export { useFirestoreQuery } from './useFirestoreQuery'
--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 // Hook
-function useLocalStorage<T>(key: string, initialValue: T) {
+export function useLocalStorage<T>(key: string, initialValue: T) {
     // State to store our value
     // Pass initial state function to useState so logic is only executed once
     const [storedValue, setStoredValue] = useState<T>(() => {
